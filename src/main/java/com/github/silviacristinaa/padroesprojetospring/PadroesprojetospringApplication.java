b28fc2ca38740d498fa557a05bf93ba2d5fd7a6a
silviacristinaa <silviacristinaalexandre1@gmail.com>
2022-11-03 14:08:57 -0300

    First Commit

---
+++ src/main/java/com/github/silviacristinaa/padroesprojetospring/PadroesprojetospringApplication.java
@@ -0,0 +1,13 @@
+package com.github.silviacristinaa.padroesprojetospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PadroesprojetospringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PadroesprojetospringApplication.class, args);
+	}
+
+}
